31d5bd2015b6bcb871b2971e9e2b52161794c4e8
bwall72 <brandonw@live.com>
2018-03-24 16:51:12 -0400

    a

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
     $var1 = $_GET['SPEED'];
     $var2 = $_GET['RPM'];
-
+    
+    var_dump($_POST);
+	
     $fileContent = "SPEED = ".$var1." RPM ".$var2."\n";
 
     $fileStatus = file_put_contents('data_file.txt', $fileContent, FILE_APPEND);
