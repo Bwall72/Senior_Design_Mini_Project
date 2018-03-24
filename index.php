03abfe1361c9f90290303afdaa3bc92cdf548b6d
bwall72 <brandonw@live.com>
2018-03-24 16:40:23 -0400

    debugging index php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     $var1 = $_GET['SPEED'];
     $var2 = $_GET['RPM'];
 
-    $fileContent = "SPEED = " .$var1 " RPM " .$var2;
+    $fileContent = "SPEED = ".$var1." RPM ".$var2."\n";
 
     $fileStatus = file_put_contents('data_file.txt', $fileContent, FILE_APPEND);
 
