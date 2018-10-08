4715e132a4cf12ccc1c2b41edda5dc07b23fffcb
bwall72 <brandonw@live.com>
2018-10-08 02:55:02 -0400

    another one

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ print_r($speed_readings);
 print_r($time_readings);
 print_r($rpm_readings);
 
-for ($i = 0; i < count($speed_readings); $i = $i + 1) {
+for ($i = 0; $i < count($speed_readings); $i = $i + 1) {
 
     
     $file_content = $time_readings[$i] . "," . $speed_readings[$i] . "," . $rpm_readings[$i] . "\n";
