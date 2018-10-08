cf72247d964b100e2fa7ec76605f922c39d45558
bwall72 <brandonw@live.com>
2018-10-08 01:45:14 -0400

    changed index to use buffered data

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
-    $speed   = $_GET['SPEED'];
-    $rpm     = $_GET['RPM'];
-    $voltage = $_GET['Voltage'];
-    $time    = $_GET['time'];;
+$speed   = $_GET['SPEED'];
+$rpm     = $_GET['RPM'];
+$time    = $_GET['time'];;
+
+var_dump($_POST);
+
+$speed_readings = explode(",", speed);
+$rpm_readings   = explode(",", rpm);
+$time_readings  = explode(",", time);
+
+foreach($speed_readings as $s and $rpm_readings as $r
+        and $time_readings as $t) {
     
-    var_dump($_POST);
-	
-    $fileContent = "Speed ".$speed." RPM ".$rpm." Voltage ".$voltage." time ".$time."\n";
-
-    $fileStatus = file_put_contents('nyc_trip.txt', $fileContent, FILE_APPEND);
-
-    if($fileStatus != false)
+    $file_content = $t . "," . $s "," . $r . "\n";
+    $file_status = file_put_contents('test_trip.txt', $fileContent, FILE_APPEND);
+    
+    if($file_status != false)
     {
         echo "Data written to file";
     }
@@ -18,4 +23,5 @@
     {
         echo "Failure";
     }
+}
 ?>
