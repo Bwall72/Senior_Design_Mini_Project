1ea8b9c159515d8a285770d3de7439041d2a6702
bwall72 <brandonw@live.com>
2018-10-08 04:01:02 -0400

    -adding buffered sketch

---
+++ Sketches/oct8/oct8.ino
@@ -0,0 +1,37 @@
+#include <OBD2UART.h>
+#include <WiFiClient.h>
+#include <HTTPClient.h>
+#include "network.h"
+#include "param.h"
+
+void
+setup()
+{
+  Serial.begin(115200);
+
+  WiFi_connect();
+
+   for ( ;; ) {
+    delay(1000);
+    byte version = obd.begin();
+
+    if (version > 0) {
+      Serial.println("Connected");
+      break;
+    }
+    else {
+      Serial.println("Adapter not connected");
+      delay(2000);
+    }
+   }
+}
+
+void 
+loop()
+{
+  if (take_reading() == 0) {
+    Serial.println("Making post");
+    HTTP_post();
+  } 
+}
+
---
+++ Sketches/oct8/param.h
@@ -0,0 +1,84 @@
+#define MAX_COUNT 100
+
+typedef unsigned char uchar;
+typedef unsigned int uint;
+
+HardwareSerial Serial1(1);
+COBD obd;
+
+const byte pids[] = {PID_SPEED, PID_RPM};
+
+int current_time = millis();
+
+String speeds[MAX_COUNT];
+String rpms[MAX_COUNT];
+String times[MAX_COUNT];
+uchar buff_loc = 0;
+
+int
+take_reading(void)
+{
+  int response[sizeof(pids)];
+  float t = millis() / 1000.;
+
+  if (obd.readPID(pids, sizeof(pids), response) == sizeof(pids)) {
+    speeds[buff_loc] = String(response[0]);
+    rpms[buff_loc] = String(response[1]);
+    times[buff_loc] = String(millis() / 1000.);
+    ++buff_loc %= MAX_COUNT;
+    return buff_loc;
+  }
+
+  return -1;
+}
+
+String
+create_url(void) 
+{
+  String url = "https://auto-tracker.azurewebsites.net/index.php/?SPEED=";
+
+  for (int i = 0; i < MAX_COUNT-1; i++) {
+    url += speeds[i] + ",";
+  }
+  url += speeds[MAX_COUNT-1] + "&RPM=";
+
+  for (int i = 0; i < MAX_COUNT-1; i++) {
+    url += rpms[i] + ",";
+  }
+  url += rpms[MAX_COUNT-1] + "&time=";
+
+  for (int i = 0; i < MAX_COUNT-1; i++) {
+    url += String(times[i]) + ",";
+  }
+  url += times[MAX_COUNT-1];
+
+  return url;
+}
+
+// Expected from network.h
+// char *ssid = "xxx";
+// char *pswd = "xxx";
+
+HTTPClient http;
+void
+WiFi_connect(void)
+{
+  WiFi.begin(ssid, pswd);
+
+  Serial.print("Connecting to " + String(ssid));
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+}
+
+void
+HTTP_post(void)
+{
+  String url = create_url();
+  http.begin(url);
+  Serial.println(url);
+  http.POST(url);
+  http.end();
+}
+
