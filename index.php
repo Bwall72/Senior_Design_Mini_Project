12e291c83e297ec0999ca23dc9ca72a59fa2a5fe
bwall72 <brandonw@live.com>
2018-10-08 02:53:14 -0400

    forgot $ again

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ print_r($rpm_readings);
 for ($i = 0; i < count($speed_readings); $i = $i + 1) {
 
     
-    $file_content = $time_readings[i] . "," . $speed_readings[i] . "," . $rpm_readings[i] . "\n";
+    $file_content = $time_readings[$i] . "," . $speed_readings[$i] . "," . $rpm_readings[$i] . "\n";
     $file_status = file_put_contents('test_trip.txt', $fileContent, FILE_APPEND);
     
     if($file_status != false)
